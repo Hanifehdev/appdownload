8b90d5247d04949a3e04426f0b0b3fa4e7f277cf
unknown <hanifeh.dev@gmail.com>
2024-12-11 17:35:08 +0330

    home-end

--- src/app/components/HomeBox.jsx
+++ src/app/components/HomeBox.jsx
@@ -5,19 +5,25 @@ import Link from 'next/link';
 const HomeBox = () => {
   return (
     <div className="mt-16">
-      <p className="text-center text-2xl font-semibold text-slate-200">
+      <p className="text-center text-2xl font-semibold text-slate-200 cursor-default">
         انتخاب سیستم عامل
       </p>
-      <div className="flex flex-col gap-4 mt-8">
-        <Link href="/android">
-          <div className="homeBox">اندروید</div>
-        </Link>
-        <Link href="/ios">
-          <div className="homeBox">آی‌او‌اس</div>
-        </Link>
-        <Link href="windows">
-          <div className="homeBox">ویندوز</div>
-        </Link>
+      <div className="grid grid-cols-12 gap-4 mt-8">
+        <div className="md:col-span-4 col-span-12">
+          <Link href="/android">
+            <div className="homeBox">اندروید</div>
+          </Link>
+        </div>
+        <div className="md:col-span-4 col-span-12">
+          <Link href="/ios">
+            <div className="homeBox">آی‌او‌اس</div>
+          </Link>
+        </div>
+        <div className="md:col-span-4 col-span-12">
+          <Link href="/windows">
+            <div className="homeBox">ویندوز</div>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/app/components/HomeTitle.jsx
+++ src/app/components/HomeTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomeTitle = () => {
   return (
-    <h2 className="font-bold text-3xl text-center">
+    <h2 className="font-bold text-3xl text-center cursor-default">
       دانلود مستقیم نرم افزار های مورد نیاز
     </h2>
   );
