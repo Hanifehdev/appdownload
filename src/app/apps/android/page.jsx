5b5abcd563f6301061d410781eea1bf38dc7e405
unknown <hanifeh.dev@gmail.com>
2024-12-12 19:21:11 +0330

    add-lowercase-func

--- src/app/apps/android/page.jsx
+++ src/app/apps/android/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+
 import BackBtn from '@/app/components/BackBtn';
 import AppName from '@/app/components/AppName';
 
@@ -7,13 +9,17 @@ const Page = async () => {
   const data = await fetch(`${process.env.API_URL}/android`);
   const apps = await data.json();
 
+  const toLowerCase = (word) => {
+    return word.toLowerCase().replace(/\s+/g, '');
+  };
+
   return (
     <div className="w-full h-screen flex flex-col justify-center">
       <AppName AppName={'اندروید'} />
       <div className="grid grid-cols-12 gap-4 mt-8">
         {apps.map((app) => (
           <div key={app.id} className="lg:col-start-5 lg:col-end-9 col-span-12">
-            <a href={app.downloadLink}>
+            <Link href={`/apps/android/${toLowerCase(app.appName)}`}>
               <div className="homeBox">
                 <div className="flex items-center justify-between w-full p-4">
                   <div className="overflow-clip rounded">
@@ -28,7 +34,7 @@ const Page = async () => {
                   <p className="text-slate-200">{app.appName}</p>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
         ))}
         <BackBtn path="/" />
