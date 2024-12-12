86b48c8e8d32eafa6234a4c9b716748a444824d4
unknown <hanifeh.dev@gmail.com>
2024-12-12 20:24:01 +0330

    last-state

--- src/app/apps/android/v2rayng/page.jsx
+++ src/app/apps/android/v2rayng/page.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import BackBtn from '@/app/components/BackBtn';
+const page = async () => {
 
-const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <section className="w-full h-screen flex flex-col items-center justify-center">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-12 gap-4 mt-8">
+            <div className="lg:col-start-5 lg:col-end-9 col-span-12">
+              {/* <a href={thisApp.downloadLink}> */}
+                <div className="homeBox">دانلود مستقیم</div>
+              {/* </a> */}
+            </div>
+            <div className="lg:col-start-5 lg:col-end-9 col-span-12">
+              <div className="homeBox">آموزش ویدئویی</div>
+            </div>
+            <BackBtn path="/apps/android/" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
