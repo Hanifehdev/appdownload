a25518bdfb92bd2e48f8c6dec3af23bd80c8035e
unknown <hanifeh.dev@gmail.com>
2024-12-14 12:33:31 +0330

    bug-fix

--- src/app/apps/android/page.jsx
+++ src/app/apps/android/page.jsx
@@ -5,12 +5,13 @@ import AppList from '@/app/components/AppList';
 import { fetchData } from '../utils/fetchData';
 
 const Page = async () => {
+  const platform = 'android';
   const apps = await fetchData('android');
 
   return (
     <div className="w-full h-screen flex flex-col justify-center">
       <AppName AppName={'اندروید'} />
-      <AppList platform={apps} />
+      <AppList platform={platform} data={apps} />
     </div>
   );
 };
