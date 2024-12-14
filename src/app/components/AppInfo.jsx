fc40f2dc9f083599ecd485ceb6eb5f9ae2fa6065
unknown <hanifeh.dev@gmail.com>
2024-12-14 14:34:48 +0330

    v1.3

---
+++ src/app/components/AppInfo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { fetchData } from '../utils/fetchData';
+
+const AppInfo = ({ imageURL, imageAlt, Name, RequireVersion }) => {
+  const data = fetchData('android');
+  return (
+    <div className="flex items-center justify-between h-16" dir="ltr">
+      <div className="flex flex-col w-full items-start justify-between">
+        <div className="font-bold text-2xl">{Name}</div>
+        <div className="font-light text-slate-300">{RequireVersion}</div>
+      </div>
+      <div className="rounded overflow-clip">
+        <Image
+          alt={imageAlt}
+          width={64}
+          height={64}
+          quality={100}
+          src={imageURL}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AppInfo;
--- src/app/components/AppPage.jsx
+++ src/app/components/AppPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import BackBtn from './BackBtn';
+
+import AppInfo from './AppInfo';
+
 const AppPage = ({ application, backValue }) => {
   const thisApp = application;
   return (
@@ -7,12 +10,22 @@ const AppPage = ({ application, backValue }) => {
       <div className="container mx-auto">
         <div className="grid grid-cols-12 gap-4 mt-8">
           <div className="lg:col-start-5 lg:col-end-9 col-span-12">
+            <AppInfo
+              imageURL={thisApp.imageURL}
+              imageAlt={thisApp.appName}
+              Name={thisApp.appName}
+              RequireVersion={thisApp.RequireVersion}
+            />
+          </div>
+          <div className="lg:col-start-5 lg:col-end-9 col-span-12">
             <a href={thisApp.downloadLink}>
               <div className="homeBox">دانلود مستقیم</div>
             </a>
           </div>
           <div className="lg:col-start-5 lg:col-end-9 col-span-12">
-            <div className="homeBox">(به زودی)آموزش ویدئویی</div>
+            <div className="homeBox hover:bg-slate-700 cursor-default">
+              آموزش ویدئویی (به زودی)
+            </div>
           </div>
           <BackBtn path={backValue} />
         </div>
