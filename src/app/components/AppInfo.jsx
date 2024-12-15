85644f32ee539deae114c57ddb9e7cc99822df5f
unknown <hanifeh.dev@gmail.com>
2024-12-15 11:21:05 +0330

    add-ios-videos

--- src/app/components/AppInfo.jsx
+++ src/app/components/AppInfo.jsx
@@ -6,7 +6,7 @@ import { fetchData } from '../utils/fetchData';
 const AppInfo = ({ imageURL, imageAlt, Name, RequireVersion }) => {
   const data = fetchData('android');
   return (
-    <div className="flex items-center justify-between h-16" dir="ltr">
+    <div className="flex items-center justify-between" dir="ltr">
       <div className="flex flex-col w-full items-start justify-between">
         <div className="font-bold text-2xl">{Name}</div>
         <div className="font-light text-slate-300">{RequireVersion}</div>
